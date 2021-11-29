ae6b971923034c1dd48834f00b0eea128b412d6c
EslamHashem <eslamhashem1213@gmail.com>
2021-11-29 19:22:47 +0200

    New Template

--- js/App.js
+++ js/App.js
@@ -1,7 +1,8 @@
 let btn = document.getElementById('btn');
+let x = document.getElementById('landing');
 
 window.onscroll = function () {
-  if (window.pageYOffset >= 907) {
+  if (window.pageYOffset >= x.offsetHeight) {
     btn.style.display = 'block';
   } else {
     btn.style.display = 'none';
@@ -11,3 +12,33 @@ window.onscroll = function () {
 btn.onclick = function () {
   window.scrollTo(0, 0);
 };
+// TypeWriter
+let i = 0;
+let txt = document.getElementById('head');
+let text = txt.innerHTML;
+txt.innerHTML = ' ';
+let speed = 100;
+
+function typeWriter() {
+  if (i < text.length) {
+    txt.innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+typeWriter();
+
+let p = 0;
+let para = document.getElementById('para');
+let graph = para.innerHTML;
+para.innerHTML = ' ';
+let velocity = 10;
+
+function typing() {
+  if (p < graph.length) {
+    para.innerHTML += graph.charAt(p);
+    p++;
+    setTimeout(typing, velocity);
+  }
+}
+typing();
